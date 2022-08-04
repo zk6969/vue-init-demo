92dbf8f21995bea2b557a4415dcce95c60558f90
zk <774511622@qq.com>
2022-08-04 20:20:39 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Test from '@/components/Test.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 Vue.component('Test', Test)
 
